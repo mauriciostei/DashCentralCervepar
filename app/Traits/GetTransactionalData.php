5a47b2ac5de3fd53d73761f3c74569a4ef14908c
Mauricio <80491400+mauriciostei@users.noreply.github.com>
2024-03-22 18:05:52 -0300

    Get with try

--- app/Traits/GetTransactionalData.php
+++ app/Traits/GetTransactionalData.php
@@ -5,6 +5,7 @@ namespace App\Traits;
 use App\Enums\CDS;
 use App\Models\Plan;
 use App\Models\Recorrido;
+use Exception;
 use Illuminate\Support\Facades\DB;
 
 trait GetTransactionalData{
@@ -47,23 +48,27 @@ trait GetTransactionalData{
             )");
 
             foreach($consulta as $line):
-                Recorrido::updateOrCreate(
-                    [
-                        'centro' => $c->value,
-                        'fecha' => date('Y-m-d'),
-                        'chofers_id' => $line->choferes_id,
-                        'movils_id' => $line->moviles_id,
-                        'puntos_id' => $line->puntos_id,
-                        'tiers_id' => $line->tiers_id,
-                        'viaje' => $line->viaje,
-                        'inicio' => $line->inicio,
-                        'target' => $line->target,
-                        'ponderacion' => $line->ponderacion,
-                    ], 
-                    [
-                        'fin' => $line->fin,
-                        'estado' => $line->estado,
-                    ]);
+                try{
+                    Recorrido::updateOrCreate(
+                        [
+                            'centro' => $c->value,
+                            'fecha' => date('Y-m-d'),
+                            'chofers_id' => $line->choferes_id,
+                            'movils_id' => $line->moviles_id,
+                            'puntos_id' => $line->puntos_id,
+                            'tiers_id' => $line->tiers_id,
+                            'viaje' => $line->viaje,
+                            'inicio' => $line->inicio,
+                            'target' => $line->target,
+                            'ponderacion' => $line->ponderacion,
+                        ], 
+                        [
+                            'fin' => $line->fin,
+                            'estado' => $line->estado,
+                        ]);
+                }catch(Exception $err){
+                    return;
+                }
             endforeach;
             
         endforeach;
