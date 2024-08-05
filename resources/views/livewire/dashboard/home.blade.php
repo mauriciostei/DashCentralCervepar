b448ea9fe8a22e2ebbd488e5bafd5a412e99c95e
Mauricio <80491400+mauriciostei@users.noreply.github.com>
2024-08-05 13:21:50 -0400

    New Fix

--- resources/views/livewire/dashboard/home.blade.php
+++ resources/views/livewire/dashboard/home.blade.php
@@ -5,7 +5,7 @@
     </div>
 
     <div class="flex-grow-1 d-flex">
-        <div class="d-flex me-1 w-100 overflow-scroll h-100 flex-grow-1">
+        <div class="d-flex me-1 w-100 overflow-y-scroll h-100 flex-grow-1">
             @livewire('dashboard.home-t-m-a.table-movil') 
         </div>
         <div class="d-flex ms-1 w-100">
